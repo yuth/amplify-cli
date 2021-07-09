21a9c1fad50e8de438dd6a39d134598f3ca5a49e
Yathi <511386+yuth@users.noreply.github.com>
2021-07-09 12:36:16 -0700

    ci: tagged release without e2e

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -2,17 +2,20 @@
 if [ -z "$CIRCLE_PULL_REQUEST" ]; then
   git config --global user.email $GITHUB_EMAIL
   git config --global user.name $GITHUB_USER
-  if [[ "$CIRCLE_BRANCH" =~ ^tagged-release\/.* ]]; then
-    # Remove tagged-release/
-    export NPM_TAG="${CIRCLE_BRANCH/tagged-release\//}"
-    # Remove /no-e2e-tests
-    export NPM_TAG="${NPM_TAG/\/no-e2e-tests/}"
-    if [ -z "$NPM_TAG" ]; then
-      echo "Tag name is missing. Name your banch with tagged-release/<tag-name>"
-      exit 1
+  if [[ "$CIRCLE_BRANCH" =~ ^tagged-release ]]; then
+    if [[ "$CIRCLE_BRANCH" =~ ^tagged-release-without-e2e-tests\/.* ]]; then
+        # Remove tagged-release-without-e2e-tests/
+      export NPM_TAG="${CIRCLE_BRANCH/tagged-release-without-e2e-tests\//}"
+    elif [[ "$CIRCLE_BRANCH" =~ ^tagged-release\/.* ]]; then
+      # Remove tagged-release/
+      export NPM_TAG="${CIRCLE_BRANCH/tagged-release\//}"
     fi
-    echo "Publishing to NPM with tag $NPM_TAG"
-    yarn publish:tag
+    if [ -z "$NPM_TAG" ]; then
+        echo "Tag name is missing. Name your banch with either tagged-release/<tag-name> or tagged-release-without-e2e-tests/<tag-name>"
+        exit 1
+      fi
+      echo "Publishing to NPM with tag $NPM_TAG"
+      yarn publish:tag
   else
     yarn publish:$CIRCLE_BRANCH
   fi
