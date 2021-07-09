3c7836ce6b8c2d944d93965a24b87e98494214b7
Yathi <511386+yuth@users.noreply.github.com>
2021-07-09 11:58:11 -0700

    ci: add support for just running e2e and tag release without e2e

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,7 +3,10 @@ if [ -z "$CIRCLE_PULL_REQUEST" ]; then
   git config --global user.email $GITHUB_EMAIL
   git config --global user.name $GITHUB_USER
   if [[ "$CIRCLE_BRANCH" =~ ^tagged-release\/.* ]]; then
+    # Remove tagged-release/
     export NPM_TAG="${CIRCLE_BRANCH/tagged-release\//}"
+    # Remove /no-e2e-tests
+    export NPM_TAG="${NPM_TAG/\/no-e2e-tests/}"
     if [ -z "$NPM_TAG" ]; then
       echo "Tag name is missing. Name your banch with tagged-release/<tag-name>"
       exit 1
